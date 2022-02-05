91bb0e85538dc8f70b72e5317da2a1930dfb177c
Felipe Brunetti <felipe.brunetti94@gmail.com>
2022-02-05 11:42:47 -0300

    feat: add EditorUser component

---
+++ src/view/EditorUser.js
@@ -0,0 +1,26 @@
+export default function EditorUser({ updateField, onSubmit, user, cancel }) {
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    onSubmit()
+  }
+  const handleChange = (fieldname) => (event) => {
+    updateField(fieldname, event.target.value)
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input required type="text" id="name" name="name" value={user} onChange={handleChange("name")} />
+        </div>
+
+        <div>
+          <label htmlFor="email">Email</label>
+          <input required type="email" id="email" name="email" value={user} onChange={handleChange("email")} />
+        </div>
+        <button name="cancel" onClick={cancel}>Cancel</button>
+        <button name="submit" type="submit">Submit</button>
+      </form>
+    </div>
+  )
+}
